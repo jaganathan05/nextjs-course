eeb1a3335a7cac1cf4b7d32b92f16241d6029603
jaganathan05 <jaganathanv888@gmail.com>
2024-06-17 14:48:03 +0530

    Prerendering the powers of Next JS

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -11,4 +11,48 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupid: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupid: 'm2',
+        },
+      },
+      {
+        params: {
+          meetupid: 'm3',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupid;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
